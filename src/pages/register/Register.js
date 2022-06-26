f8bff99866aa7a2275cf542aa68d756e47f19ba0
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-06-26 22:07:50 +0530

    added password form into our register page

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './Register.scss'
 
 export default function Register() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const emailRef = useRef()
+    const passwordRef = useRef()
+
+    const handleStart = () => {
+        setEmail(emailRef.current.value)
+    }
+    const handleFinish = () => {
+        setPassword(passwordRef.current.value)
+    }
+
     return (
         <div className='register'>
             <div className="top">
@@ -16,10 +29,17 @@ export default function Register() {
                 <h1>Unlimited movies, TV shows, and more.</h1>
                 <h2>Watch anywhere. Cancel anytime.</h2>
                 <p>Ready to watch? Enter Your email to create or restart your membership.</p>
-                <div className="input">
-                    <input type="email" placeholder='Email Address' name="" id="" />
-                    <button className="registerButton">Get Started</button>
-                </div>
+
+                {!email ? (<div className="input">
+                    <input type="email" placeholder='Email Address' name="" id="" ref={emailRef} />
+                    <button className="registerButton" onClick={() => handleStart()}>Get Started</button>
+                </div>)
+                    : (<form className="input">
+                        <input type="password" placeholder='Password' name="" id="" ref={passwordRef} />
+                        <button className="registerButton" onClick={() => handleFinish()}>Start</button>
+
+                    </form>)
+                }
             </div>
         </div>
     )
