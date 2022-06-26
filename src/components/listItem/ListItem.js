3b926bfb75d9e88e86fdd94d873610aee51aa876
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-06-26 21:14:43 +0530

    list item component completed with all features

--- src/components/listItem/ListItem.js
+++ src/components/listItem/ListItem.js
@@ -19,14 +19,14 @@ export default function ListItem({ index }) {
                     <video src={trailer} autoPlay={true} loop />
                     <div className="itemInfo">
                         <div className="icons">
-                            <PlayArrow />
-                            <Add />
-                            <ThumbUpAltOutlined />
-                            <ThumbDownOutlined />
+                            <PlayArrow className='icon' />
+                            <Add className='icon' />
+                            <ThumbUpAltOutlined className='icon' />
+                            <ThumbDownOutlined className='icon' />
                         </div>
                         <div className="itemInfoTop">
                             <span>1 hour 14 mins</span>
-                            <span className=''>17+</span>
+                            <span className='limit'>17+</span>
                             <span>1998</span>
                         </div>
                         <div className="description">
