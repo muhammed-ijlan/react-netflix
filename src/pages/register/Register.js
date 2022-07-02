ff2472089c28bc4a1386cdf917559e437ef7ebd1
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-07-02 17:30:22 +0530

    updated register page

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,19 +1,35 @@
 import { useRef } from "react";
 import { useState } from "react";
 import "./Register.scss";
+import axios from 'axios'
+import { useHistory } from 'react-router-dom'
 
 export default function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [username, setUsername] = useState("")
+    const history = useHistory()
 
     const emailRef = useRef();
     const passwordRef = useRef();
+    const usernameRef = useRef();
 
     const handleStart = () => {
         setEmail(emailRef.current.value);
     };
-    const handleFinish = () => {
+    const handleFinish = async (e) => {
+        e.preventDefault();
+
         setPassword(passwordRef.current.value);
+        setUsername(usernameRef.current.value)
+        try {
+            await axios.post("auth/register", { email, username, password })
+            history.push('/login')
+
+        } catch (err) {
+            console.log(err);
+        }
+
     };
     return (
         <div className="register">
@@ -42,6 +58,7 @@ export default function Register() {
                     </div>
                 ) : (
                     <form className="input">
+                        <input type="username" placeholder="username" ref={usernameRef} />
                         <input type="password" placeholder="password" ref={passwordRef} />
                         <button className="registerButton" onClick={handleFinish}>
                             Start
