7a1446b46c5c4e3c08c7af6d9655d6d0d2c04f66
Muhammed Ijlan <ijlanijlu580@gmail.com>
2022-07-03 23:21:57 +0530

    project upgraded

--- src/authContext/apiCalls.js
+++ src/authContext/apiCalls.js
@@ -1,14 +1,13 @@
-
+import axios from "axios";
 import { axiosInstance } from "../config";
-import { loginFailure, loginStart, loginSuccess } from "./AuthActions"
+import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 
 export const login = async (user, dispatch) => {
-    dispatch(loginStart)
-    try {
-        const res = await axiosInstance.post("auth/login", user);
-        dispatch(loginSuccess(res.data))
-
-    } catch (err) {
-        dispatch(loginFailure())
-    }
-}
+  dispatch(loginStart());
+  try {
+    const res = await axiosInstance.post("auth/login", user);
+    dispatch(loginSuccess(res.data));
+  } catch (err) {
+    dispatch(loginFailure());
+  }
+};
